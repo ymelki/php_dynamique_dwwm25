7cd3cc627553474f3fe9b42659ed5ddf20411006
Yoel Melki <ymelki@gmail.com>
2022-02-09 11:05:39 +0100

    co

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ $article=array();
 // AFFICHER UN TABLEAU TECHNIQUE / classique
 //var_dump($article);
 
+// http://localhost/index.php?votrvar=efdsfdsf32432&tr=GFD
 
  
 var_dump($_GET); 
@@ -32,7 +33,21 @@ foreach($utilisateur as $key => $value){
 
 2 VARIABLE LOCALE  : limité à une fonction
 3 VARIABLE GLOBAL  : limité à la page
-4 VARIABLE SUPER GLOBAL : plus qu'une page
+4 VARIABLE SUPER GLOBAL : plus qu'une page 
+$_GET (URL)
+$_POST 
 */ 
 
 ?>
+<br />
+<a href="votrepage.php?id=23">Article 23</a>
+
+<form method="POST" action="reception_form.php">
+    VOICI LA VALEUR QUE JE POSTE PAR PHP
+    <br />
+    <input type="text" value="" name="nom"  />
+    <br />
+    <input type="password" value="" name="pwd"  />
+    <br />
+    <input type="submit" value="Envoyer" />
+</form>
